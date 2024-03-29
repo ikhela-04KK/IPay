7c6897fc0d40cfb0fda4cfadb11493e3b1b09e4c
kankoffi36 <kankoffi36@gmail.com>
2024-03-29 23:23:38 +0000

    my modif setup

---
+++ lib/ui/util/constant.dart
@@ -0,0 +1,141 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const COLOR_PRIMARY = Colors.deepOrangeAccent;
+const COLOR_ACCENT = Colors.orange;
+const COLOR_BACKGROUND_DARK = Color(0xFF171822);
+const COLOR_BACKGROUND = Colors.white;
+const COLOR_BACKGROUND_LIGHT = Color(0xFFF1F3F6);
+
+// Définir le schéma de couleur avec les couleurs nécessaires
+ColorScheme colorScheme = const ColorScheme(
+  brightness: Brightness.light, // Spécifie la luminosité, vous pouvez utiliser Brightness.dark pour le thème sombre
+  primary: COLOR_PRIMARY, // Couleur primaire
+  onPrimary: Colors.white, // Couleur du texte sur la couleur primaire
+  secondary: COLOR_ACCENT, // Couleur secondaire
+  onSecondary: Colors.white, // Couleur du texte sur la couleur secondaire
+  error: Colors.red, // Couleur d'erreur
+  onError: Colors.white, // Couleur du texte sur la couleur d'erreur
+  background: COLOR_BACKGROUND_DARK, // Couleur d'arrière-plan
+  onBackground: Colors.white, // Couleur du texte sur la couleur d'arrière-plan
+  surface: COLOR_BACKGROUND, // Couleur de surface
+  onSurface: Color(0xFF171822), // Couleur du texte sur la couleur de surface
+);
+
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: COLOR_PRIMARY,
+  cardColor: const Color(0xffF1F3F6),
+  iconTheme: const IconThemeData(
+    color: Color(0xFF3A4276),
+  ),
+  textTheme: TextTheme(
+    labelLarge: GoogleFonts.poppins(
+      fontSize: 15,
+      height: 1.6,
+      color: const Color(0xff212330),
+      fontWeight: FontWeight.w600,
+    ),
+    bodyMedium: GoogleFonts.poppins(
+      fontSize: 12,
+      color: const Color(0xff1B1D28),
+      fontWeight: FontWeight.w500,
+    ),
+    bodyLarge: GoogleFonts.poppins(
+      height: 1.6,
+      fontSize: 12,
+      color: const Color(0xff7b7f9e),
+      fontWeight: FontWeight.w400,
+    ),
+    displayMedium: GoogleFonts.poppins(
+      fontSize: 24,
+      color: const Color(0xff171822),
+      fontWeight: FontWeight.w600,
+    ),
+    displaySmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: const Color(0xff3A4276),
+      fontWeight: FontWeight.w800,
+    ),
+    headlineMedium: GoogleFonts.poppins(
+      fontSize: 15,
+      color: const Color(0xff3A4276),
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: Colors.black,
+      fontWeight: FontWeight.w600,
+    ),
+    titleLarge: GoogleFonts.poppins(
+      fontSize: 20,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+    ),
+    titleMedium: GoogleFonts.poppins(
+      fontSize: 18,
+      color: Colors.black,
+      fontWeight: FontWeight.w700,
+    ),
+  ), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: COLOR_BACKGROUND_LIGHT).copyWith(background: COLOR_BACKGROUND),
+);
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  iconTheme: const IconThemeData(
+    color: Color(0xff7b7f9e),
+  ),
+  cardColor: const Color(0xFF212330),
+  textTheme: TextTheme(
+    bodyMedium: GoogleFonts.poppins(
+      fontSize: 12,
+      color: const Color(0xffffffff),
+      fontWeight: FontWeight.w500,
+    ),
+    bodyLarge: GoogleFonts.poppins(
+      height: 1.6,
+      fontSize: 12,
+      color: const Color(0xff7b7f9e),
+      fontWeight: FontWeight.w400,
+    ),
+    labelLarge: GoogleFonts.poppins(
+      fontSize: 15,
+      height: 1.6,
+      color: const Color(0xff212330),
+      fontWeight: FontWeight.w600,
+    ),
+    displayMedium: GoogleFonts.poppins(
+      fontSize: 24,
+      color: const Color(0xFFFFFFFF),
+      fontWeight: FontWeight.w600,
+    ),
+    displaySmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: const Color(0xFFFFFFFF),
+      fontWeight: FontWeight.w800,
+    ),
+    headlineMedium: GoogleFonts.poppins(
+      fontSize: 15,
+      color: const Color(0xff7b7f9e),
+      fontWeight: FontWeight.w600,
+    ),
+    headlineSmall: GoogleFonts.poppins(
+      fontSize: 22,
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+    ),
+    titleLarge: GoogleFonts.poppins(
+      fontSize: 20,
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+    ),
+    titleMedium: GoogleFonts.poppins(
+      fontSize: 18,
+      color: Colors.white,
+      fontWeight: FontWeight.w700,
+    ),
+  ), colorScheme: colorScheme,
+);
